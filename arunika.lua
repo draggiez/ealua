4df2d160544c7c6830403637858950b8749f13b8
draggiez <146788137+draggiez@users.noreply.github.com>
2025-08-16 20:32:03 +0700

    Update arunika.lua

--- arunika.lua
+++ arunika.lua
@@ -35,7 +35,7 @@ local function respawnAndWait()
     end
     local char = player.Character or player.CharacterAdded:Wait()
     char:WaitForChild("HumanoidRootPart")
-    task.wait(4)
+    task.wait(5)
 end
 
 -- Touch part with delay
