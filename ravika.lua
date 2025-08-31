869fd7b8e2211c91710041b360b451fe01a5f702
draggiez <146788137+draggiez@users.noreply.github.com>
2025-08-31 19:13:16 +0700

    Update ravika.lua

--- ravika.lua
+++ ravika.lua
@@ -5,8 +5,6 @@ local TweenService = game:GetService("TweenService")
 --local teleportPos = Vector3.new(61, 93, -113)
 local teleportPos = Vector3.new(152.98, 82.87, 103.76)
 local loopRunning = false
-local hrps = character:WaitForChild("HumanoidRootPart")
-local originalCFrame = hrps.CFrame
 
 -- CP
 local checkpointsCamera = {
@@ -344,6 +342,8 @@ btnStartStop.MouseButton1Click:Connect(function()
         btnStartStop.Text = "Stop"
         runner = coroutine.create(function()
             while loopRunning do
+				local hrp, char = getHRP()
+				local originalCFrame = hrp.CFrame
 				local start = workspace:FindFirstChild("StartTimeTrigger")
 				if start and start:IsA("BasePart") then
 	    			touchPart(start)
@@ -374,7 +374,7 @@ btnStartStop.MouseButton1Click:Connect(function()
     				Enum.EasingDirection.Out
 				)
 				local goal = {CFrame = CFrame.new(teleportPos)}
-                local hrp, char = getHRP()
+                
                 if hrp then
 					local tween = TweenService:Create(hrp, tweenInfo, goal)
 					tween:Play()
@@ -387,8 +387,10 @@ btnStartStop.MouseButton1Click:Connect(function()
 				-- Jalankan render ke semua koordinat
 				for _, pos in ipairs(checkpointsCamera) do
     				renderAtPosition(pos)
-				end		
-				hrps.CFrame = originalCFrame
+				end
+						
+				hrp.CFrame = originalCFrame
+				
                 task.spawn(function() logLabel.Text = "Waiting for Checkpoint to load..." end)
 				task.wait(1)
     --             local checkpointsFolder
