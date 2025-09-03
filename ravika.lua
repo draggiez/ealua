11958dc79b15672245661b3a8308fa7616124d91
draggiez <146788137+draggiez@users.noreply.github.com>
2025-09-03 08:25:39 +0700

    Update ravika.lua

--- ravika.lua
+++ ravika.lua
@@ -139,7 +139,7 @@ end
 
 --============== TWEEN =================--
 local function tweenHRP(hrp, targetCFrame)
-	local tweenInfo = TweenInfo.new(5, Enum.EasingStyle.Quad, Enum.EasingDirection.Out)
+	local tweenInfo = TweenInfo.new(5,  Enum.EasingStyle.Linear, Enum.EasingDirection.Out)
 	local tween = TweenService:Create(hrp, tweenInfo, {CFrame = targetCFrame})
 	tween:Play()
 	tween.Completed:Wait()
